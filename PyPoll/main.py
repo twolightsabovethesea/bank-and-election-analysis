e8f5ee806ef86da03a5b4cd4ec85c149ff963e34
Meredith Hopkins <twolightsabovethesea@gmail.com>
2021-06-29 23:20:02 -0400

    added documentation and defined some variables

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,9 +1,20 @@
+#imports needed modules
 import csv
 import os
 
+#sets variables for data to be analyzed
+vote_total = 0
+candidate_list = []
+
+#imports csv containing data for project
 csvpath = os.path.join('Resources', 'election_data.csv')
 
+#opens and reads csv file
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
     csvheader = next(csvreader)
 
+    
+
+
+    
