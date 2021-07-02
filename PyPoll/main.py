06b5c3fd17c5dfa4f5c4b314b4fb7adb7b399db7
Meredith Hopkins <twolightsabovethesea@gmail.com>
2021-07-02 19:48:14 -0400

    transferred new code to main.py, deleted test code file, added documentation

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,14 +6,8 @@ import os
 vote_total = 0
 candidate_list = []
 candidate_dict = {}
-Khan = 0
-Correy = 0
-Li = 0
-OTooley = 0
-KhanP = 0
-CorreyP = 0
-LiP = 0
-OTooleyP = 0
+winnercount = 0
+output_dict = {}
 
 
 #imports csv containing data for project
@@ -28,50 +22,47 @@ with open(csvpath) as csvfile:
         #counts total number of votes
         vote_total += 1
         #creates a list of candidate names
-        if row[2] not in candidate_list:
-            candidate_list.append(row[2])
-        #total the votes for each candidate
-        if row[2] == "Khan":
-            Khan += 1
-        elif row[2] == "Correy":
-            Correy += 1
-        elif row[2] == "Li":
-            Li += 1
-        elif row[2] == "O'Tooley":
-            OTooley += 1
+        name = row[2]
+        if name not in candidate_list:
+            candidate_list.append(name)
+            candidate_dict[name] = 0
+        candidate_dict[name] = candidate_dict[name] + 1
 
-#Calculate percentage of vote for each candidate
-KhanP = round((Khan/vote_total)*100, 3)
-CorreyP = round((Correy/vote_total)*100, 3)
-LiP = round((Li/vote_total)*100, 3)
-OTooleyP = round((OTooley/vote_total)*100, 3)
+#finds the percentage of votes for each candidate and the total number of votes for each candidate
+for i in candidate_dict:
+    tally = candidate_dict.get(i)
+    prcnt = round((tally/vote_total) * 100, 3)
+    output_dict[i] = [prcnt, tally] 
 
-    
+    if (tally > winnercount):
+        winnercount = tally
+        winner = i
 
-#Prints election results to terminal
-print(f"Election Results\n---------------------------- \n")
-print(f"Total Votes: {vote_total}\n---------------------------- \n")
-print(f"Khan: {KhanP}% ({Khan})\n")
-print(f"Correy: {CorreyP}% ({Correy})\n")
-print(f"Li: {LiP}% ({Li})\n")
-print(f"O'Tooley: {OTooleyP}% ({OTooley})\n")
+#Prints analysis to Terminal
+
+print(f"Election Results")
 print(f"---------------------------- \n")
-print(f"Winner: \n")
+print(f"Total Votes: {vote_total}\n")
 print(f"---------------------------- \n")
+for k,v in output_dict.items():
+    print(f"{k}: {v[0]}% ({v[1]})\n")
+
+print(f"---------------------------- \n")
+print(f"Winner: {winner}\n")
+print(f"---------------------------- \n")
+
 
 #writes results to a txt file
 
 outputpath = os.path.join("analysis","analysis.txt")
 
-# with open (outputpath, 'w') as file:
-#     file.write(f"Election Results")
-#     file.write(f"\n----------------------------")
-#     file.write(f"\nTotal Votes: {vote_total}")
-#     file.write(f"\n----------------------------")
-#     file.write(f"\nKhan: {KhanP}% ({Khan})")
-#     file.write(f"\nCorrey: {CorreyP}% ({Correy})")
-#     file.write(f"\nLi: {LiP}% ({Li})")
-#     file.write(f"\nO'Tooley: {OTooleyP}% ({OTooley})")
-#     file.write(f"\n----------------------------")
-#     file.write(f"\nWinner: ")
-#     file.write(f"\n----------------------------")
+with open (outputpath, 'w') as file:
+    file.write(f"Election Results")
+    file.write(f"\n----------------------------")
+    file.write(f"\nTotal Votes: {vote_total}")
+    file.write(f"\n----------------------------")
+    for k,v in output_dict.items():
+        file.write(f"\n{k}: {v[0]}% ({v[1]})")
+    file.write(f"\n----------------------------")
+    file.write(f"\nWinner: {winner}")
+    file.write(f"\n----------------------------")
